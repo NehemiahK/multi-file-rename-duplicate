b6120dea5f311b5b01dae379ba048a336853ff19
Nehemiah Kivelevitz <nlk613@yahoo.com>
2023-09-28 23:05:18 +0300

    fix async error

--- app.js
+++ app.js
@@ -26,39 +26,49 @@ app.post('/upload', async (req, res) => {
   const csvFile = req.files.csvFile;
   const pdfName = req.body.pdfName || 'Document'; // Default PDF name if not provided
 
-  // Process the CSV file
   const pdfDuplicates = [];
 
-  fs.createReadStream(csvFile.tempFilePath)
-    .pipe(csvParser())
-    .on('data', (row) => {
-      const filename = row.filename;
-      const uniqueFilename = `${filename} - ${pdfName}.pdf`;
-      const pdfDoc = PDFLib.PDFDocument.create();
-      pdfDoc.addPage([612, 792]); // Letter size page
-      const pdfBytes = await pdfDoc.save();
-      pdfDuplicates.push({ name: uniqueFilename, data: pdfBytes });
-    })
-    .on('end', () => {
-      // Create a zip file containing duplicated PDFs
-      const zip = new JSZip();
-      pdfDuplicates.forEach((pdf) => {
-        zip.file(pdf.name, pdf.data);
-      });
+  try {
+    // Read the PDF file content
+    const pdfData = fs.readFileSync(pdfFile.tempFilePath);
 
-      // Generate the zip file
-      zip
-        .generateAsync({ type: 'nodebuffer' })
-        .then((content) => {
-          // Send the zip file as a response
-          res.setHeader('Content-Type', 'application/zip');
-          res.setHeader('Content-Disposition', 'attachment; filename=pdf_duplicates.zip');
-          res.send(content);
-        })
-        .catch((err) => {
-          res.status(500).json({ error: 'Failed to create the zip file' });
+    // Process the CSV file
+    fs.createReadStream(csvFile.tempFilePath)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        const filename = row.filename;
+        const uniqueFilename = `${filename} - ${pdfName}.pdf`;
+
+        // Clone the PDF by creating a new PDF document and adding pages
+        const pdfDoc = PDFLib.PDFDocument.create();
+        const pdfBytes = await pdfDoc.addPage(PDFLib.PDFPage.create([612, 792])); // Letter size page
+        pdfBytes.copyPages(pdfData);
+
+        pdfDuplicates.push({ name: uniqueFilename, data: pdfDoc.save() });
+      })
+      .on('end', () => {
+        // Create a zip file containing duplicated PDFs
+        const zip = new JSZip();
+        pdfDuplicates.forEach((pdf) => {
+          zip.file(pdf.name, pdf.data);
         });
-    });
+
+        // Generate the zip file
+        zip
+          .generateAsync({ type: 'nodebuffer' })
+          .then((content) => {
+            // Send the zip file as a response
+            res.setHeader('Content-Type', 'application/zip');
+            res.setHeader('Content-Disposition', 'attachment; filename=pdf_duplicates.zip');
+            res.send(content);
+          })
+          .catch((err) => {
+            res.status(500).json({ error: 'Failed to create the zip file' });
+          });
+      });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to read the PDF file' });
+  }
 });
 
 app.listen(port, () => {
